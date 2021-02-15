2b0e2afc9079562d2b17ecaa229f02cbe78a2f1b
William Connelly <Williamconnelly.dev@gmail.com>
2021-02-14 16:51:31 -0800

    Updated ListNode to include all API fields. Added fields to User List Request.

--- src/app/services/mal.service.ts
+++ src/app/services/mal.service.ts
@@ -37,6 +37,42 @@ export class MALService {
 
   private list = new BehaviorSubject<UserList>(null);
 
+  // Included in getUserList request
+  private enabledRequestFields = [
+    'id',
+    'title',
+    'main_picture',
+    'alternative_titles',
+    'start_date',
+    'end_date',
+    'synopsis',
+    'mean',
+    'rank',
+    'popularity',
+    'num_list_users',
+    'num_scoring_users',
+    'nsfw',
+    'created_at',
+    'updated_at',
+    'media_type',
+    'status',
+    'genres',
+    'my_list_status',
+    'num_episodes',
+    'start_season',
+    'broadcast',
+    'source',
+    'average_episode_duration',
+    'rating',
+    'pictures',
+    'background',
+    'related_anime',
+    'related_manga',
+    'recommendations',
+    'studios',
+    'statistics'
+  ];
+
   constructor(
     private _http: HttpClient, 
     private _ipc: IPCService,
@@ -102,8 +138,6 @@ export class MALService {
     this._ipc.renderer.send('store-token', this.malTokenInfo.refresh_token);
   }
 
-
-
   // Check for refresh token and update Access Token in service
   private refreshAccessToken() {
     this._ipc.renderer.send('refresh-token');
@@ -160,7 +194,8 @@ export class MALService {
     const headers = {
       headers: new HttpHeaders().set('Authorization', `Bearer ${this.malTokenInfo.access_token}`)
     };
-    const url = `${baseUrl}?fields=list_status&limit=999`;
+    const includedFields = this.enabledRequestFields.toString();
+    const url = `${baseUrl}?fields=${includedFields}&nsfw=true&limit=999`;
     return this._http.get<UserList>(url, headers);
   }
 
--- src/app/types/mal-types.ts
+++ src/app/types/mal-types.ts
@@ -1,13 +1,13 @@
-export interface ListNode {
-  node: {
-    id: number;
-    main_picture: {
-      large: string;
-      medium: string;
-    },
-    title: string;
-  }
-};
+// export interface ListNode {
+//   node: {
+//     id: number;
+//     main_picture: {
+//       large: string;
+//       medium: string;
+//     },
+//     title: string;
+//   }
+// };
 
 export interface ListStatus {
   is_rewatching: boolean;
@@ -21,3 +21,82 @@ export interface UserList {
   data: {node: ListNode, list_status: ListStatus}[];
   paging: object;
 };
+
+export interface MALImage {
+  medium: string;
+  large: string;
+}
+
+export interface RelatedNode {
+  node: {
+    id: number;
+    title: string;
+    main_picture: MALImage
+  };
+  relation_type: string;
+  relation_type_formatted: string;
+}
+
+export interface RecommendedNode {
+  node: {
+    id: number;
+    title: string;
+    main_picture: MALImage
+  };
+  num_recommendations: number;
+}
+
+export interface ListNode {
+  id: string;
+  title: string;
+  main_picture: MALImage;
+  alternative_titles: {
+    synonyms: string[],
+    en: string,
+    ja: string
+  };
+  start_date: string;
+  end_date: string;
+  synopsis: string;
+  mean: number;
+  rank: number;
+  popularity: number;
+  num_list_users: number;
+  num_scoring_users: number;
+  nsfw: string;
+  created_at: string;
+  updated_at: string;
+  media_type: string;
+  status: string;
+  genres: Array<{ id: number, name: string }>;
+  my_list_status: ListStatus;
+  num_episodes: number;
+  start_season: {
+    year: number,
+    season: string
+  };
+  broadcast: {
+    day_of_the_week: string,
+    start_time: string
+  };
+  source: string;
+  average_episode_duration: number;
+  rating: string;
+  pictures: MALImage[];
+  background: string;
+  related_anime: RelatedNode[];
+  related_manga: Array<any> //TODO: Type
+  recommendations: RecommendedNode[];
+  studios: Array<{ id: number; name: string }>;
+  statistics: {
+    status: {
+      watching: string,
+      completed: string,
+      on_hold: string,
+      dropped: string,
+      plan_to_watch: string
+    };
+    num_list_users: number;
+  }
+
+}
