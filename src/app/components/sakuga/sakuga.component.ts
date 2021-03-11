39940e2d3d22a1a8a762a7ec09d67762e61d5d49
William Connelly <Williamconnelly.dev@gmail.com>
2021-03-10 20:22:13 -0800

    Added click-out behavior to media modal view

--- src/app/components/sakuga/sakuga.component.ts
+++ src/app/components/sakuga/sakuga.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, Input, OnDestroy, OnInit, QueryList, Renderer2, ViewChild, ViewChildren } from '@angular/core';
 import { BehaviorSubject, Subject } from 'rxjs';
 import { filter, take, takeUntil } from 'rxjs/operators';
 import { DataService } from 'src/app/services/data-service/data-service';
@@ -10,7 +10,7 @@ import { Post } from 'src/app/types/sakuga-types';
   templateUrl: './sakuga.component.html',
   styleUrls: ['./sakuga.component.css']
 })
-export class SakugaComponent implements OnInit, OnDestroy {
+export class SakugaComponent implements OnInit, OnDestroy, AfterViewInit {
 
   @Input() embedded = false;
 
@@ -22,9 +22,30 @@ export class SakugaComponent implements OnInit, OnDestroy {
 
   @ViewChild('Modal', {static: false}) modal: ElementRef<HTMLElement>;
 
+  @ViewChild('ModalContent', {static: false}) modalContent: ElementRef<HTMLElement>;
+
+  // @ViewChild('ModalContent', {static: false}) set modalContent(content: ElementRef<HTMLElement>) {
+  //   if (content) {
+  //     this._modalContent = content;
+  //     console.log('Apply Listener');
+  //     this.modalListener = this._renderer.listen('window', 'click', (event: Event) => {
+  //       if (event.target !== this._modalContent.nativeElement && this.currentPost) {
+  //         this.closeModal();
+  //       } 
+  //     });
+  //   } else {
+  //     if (this.modalListener) {
+  //       console.log('Remove Listener');
+  //       this.modalListener();
+  //     }
+  //   }
+  // }
+
+  // @ViewChildren('ModalContent') modalContent: QueryList<ElementRef<HTMLElement>>;
+
   @ViewChild('SakugaContainer', {static: false}) sakugaContainer: ElementRef<HTMLElement>;
 
-  constructor(private _sakuga: SakugaService, private _data: DataService) {
+  constructor(private _sakuga: SakugaService, private _data: DataService, private _renderer: Renderer2) {
     this._sakuga.getPosts().pipe(
       filter(posts => !!posts),
       takeUntil(this._destroy$)
@@ -37,13 +58,26 @@ export class SakugaComponent implements OnInit, OnDestroy {
         console.error('Failed to fetch Sakuga', err);
       }
     )
-
   }
 
   ngOnInit() {
 
   }
 
+  @HostListener('window:click', ['$event']) onClick(e: MouseEvent) {
+    if (this.modalContent) {
+      const target = e.target as HTMLElement;
+      // Target is not the Modal Content or a child of Modal Content && Was not the preview div opening the modal && currentPost exists
+      if (target !== this.modalContent.nativeElement && !target.classList.contains('modal-field') && !this.modalContent.nativeElement.contains(target) && this.currentPost) {
+        this.closeModal();
+      }
+    }
+  }
+
+  ngAfterViewInit() {
+
+  }
+  
   public getSourceDisplay(source: string): string {
     return (['op','ed','http','pv'].some(src => source.toLowerCase().includes(src))) ? 'Source' : 'Episode';
   }
